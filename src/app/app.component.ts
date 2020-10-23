ca35352799b50fb45cd9e391036c7e2286f685d1
Rahul <rmanwanim@gmail.com>
2020-10-23 21:10:17 +0200

    o9o

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'My-Formulario-Project';
+
+erDNI = '^[0-9]{8}[A-Za-z]$';
+mostrarInfo = ""
+
+EnviarDatos(nombre, dni, Mensaje){
+
+if(nombre.value == ""){
+  alert("El nombre esta vacio")
+}
+if(dni.match(this.erDNI)){
+    this.mostrarInfo="Correct"
+  }
+if(Mensaje.value.lenght <= 2){
+   this.mostrarInfo= "Incorrecto"
+}
+  }
 }
